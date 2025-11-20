2e7b8c3af3897458c5e7f559ecf3daaebaacbbcf
Welton Santos <welton.santos@icoutech.com>
2025-11-19 22:05:51 -0300

    icoufootball-20251119-2205

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -1,1 +1,10 @@
-;
+import { initializeApp, getApps, getApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-firestore.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-analytics.js";
+import { firebaseConfig } from "../../config/firebase-config.js";
+
+const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const analytics = getAnalytics(app);
